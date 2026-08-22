172a9df7d0bcb1cf82a52102fcd64d7fc6e4d1a9
Nobert167 <nobertlangat0@gmail.com>
2026-08-22 10:39:23 +0300

    update: app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Navbar } from "@/components/navbar";
 import { Hero } from "@/components/hero";
 import { About } from "@/components/about";
 import { Projects } from "@/components/projects";
+import { Testimonials } from "@/components/testimonials";
 import { Skills } from "@/components/skills";
 import { Experience } from "@/components/experience";
 import { Contact, Footer } from "@/components/contact";
@@ -14,6 +15,7 @@ export default function Home() {
         <Hero />
         <About />
         <Projects />
+        <Testimonials />
         <Experience />
         <Skills />
         <Contact />
